b17709e7855ff46910af2403d05b2e438cd20ed8
RubenPants <broekxruben@gmail.com>
2020-04-22 22:54:17 +0200

    Minor updates

--- experiment6.bash
+++ experiment6.bash
@@ -8,9 +8,10 @@ cpu=0;  # Number of unused CPUs
 # Run the program
 while true
 do
-  python3 experiment6.py --topology_id=$topology --batch=$batch --unused_cpu=$cpu;
+#  python3 experiment6.py --topology_id=$topology --batch=$batch --unused_cpu=$cpu;
+  python3 experiment6_2.py --topology_id=$topology --batch=$batch --unused_cpu=$cpu;
 
-  git add .;
-  git commit -m "Evaluated $batch genomes in experiment6";
-  git push;
+#  git add .;
+#  git commit -m "Evaluated $batch genomes in experiment6";
+#  git push;
 done
