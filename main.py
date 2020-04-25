88bcb6085ce109879d3ea864f1d6372e45a51682
RubenPants <broekxruben@gmail.com>
2020-04-25 12:41:27 +0200

    ran experiment3 on v34

--- main.py
+++ main.py
@@ -288,24 +288,24 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     
     # Main methods
-    parser.add_argument('--train', type=bool, default=False)
-    parser.add_argument('--train_overview', type=bool, default=False)
+    parser.add_argument('--train', type=bool, default=True)
+    parser.add_argument('--train_overview', type=bool, default=True)
     parser.add_argument('--blueprint', type=bool, default=False)
     parser.add_argument('--trace', type=bool, default=False)  # Keep it False
     parser.add_argument('--trace_fit', type=bool, default=True)
     parser.add_argument('--evaluate', type=bool, default=False)
     parser.add_argument('--genome', type=bool, default=True)
-    parser.add_argument('--monitor', type=bool, default=True)
+    parser.add_argument('--monitor', type=bool, default=False)
     parser.add_argument('--gru_analysis', type=bool, default=False)
     parser.add_argument('--live', type=bool, default=False)
     
     # Extra arguments
-    parser.add_argument('--iterations', type=int, default=1)
-    parser.add_argument('--experiment', type=int, default=6)
+    parser.add_argument('--iterations', type=int, default=50)
+    parser.add_argument('--experiment', type=int, default=3)
     parser.add_argument('--unused_cpu', type=int, default=2)
-    parser.add_argument('--version', type=int, default=11)
+    parser.add_argument('--version', type=int, default=34)
     parser.add_argument('--debug', type=bool, default=False)
-    parser.add_argument('--use_backup', type=bool, default=True)
+    parser.add_argument('--use_backup', type=bool, default=False)
     args = parser.parse_args()
     
     # Load in current config-file
@@ -314,12 +314,12 @@ if __name__ == '__main__':
     # config.bot.angular_dir = [True, False]
     config.bot.dist_enabled = True
     config.evaluation.fitness = D_DISTANCE
-    config.genome.rnn_prob_gru = 0.6
+    # config.genome.rnn_prob_gru = 0.6
     config.update()
     
     # Setup the population
     pop = Population(
-            name='topology_1_1',
+            name='NEAT/v34',
             # name=get_name(cfg=config, version=args.version),
             # folder_name='test',
             folder_name=get_folder(args.experiment),
