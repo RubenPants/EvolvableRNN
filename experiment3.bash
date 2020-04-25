d1a9b39b8bf2f6e12ce2b99691bd4758ccfcbdd8
RubenPants <broekxruben@gmail.com>
2020-04-25 16:54:06 +0200

    Update experiment3.bash

--- experiment3.bash
+++ experiment3.bash
@@ -5,13 +5,13 @@ iter=100;  # Number of training-iterations each loop
 cpu=1;  # Number of unused CPUs
 
 # Run the program
-for v in {35..50}
+for v in {4,}
   do
   for i in {1..5}
   do
-    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
 #    python3 experiment3.py --prob_gru=0.6 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0.6 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0.6 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
 #    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0.6 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
 #    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
   done
