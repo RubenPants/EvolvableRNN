71ffa6db63182b523078ed43bc21dbbd0132bbea
RubenPants <broekxruben@gmail.com>
2020-04-26 19:06:39 +0200

    Update experiment1.bash

--- experiment1.bash
+++ experiment1.bash
@@ -2,26 +2,26 @@
 
 # Default parameters
 iter=100;  # Number of training-iterations each loop
-cpu=2;  # Number of unused CPUs
+cpu=1;  # Number of unused CPUs
 
-# Run the program  TODO: Check if 35 is done!
-for v in {35,}
+# Run the program
+for v in {1..10}
 do
   for i in {1..5}
   do
-#    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
     python3 experiment1.py --prob_gru=0.6 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
 #    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0.6 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
 #    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0.6 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
+    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
   done
-#  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
+  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
   python3 experiment2.py --prob_gru=0.6 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
 #  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0.6 --prob_gru_nu=0 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
 #  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0.6 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
-#  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0.6 --version=$v --unused_cpu=$cpu;
+  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0.6 --version=$v --unused_cpu=$cpu;
 
-#  git add .;
-#  git commit -m "Evaluated experiment1&2 (version $v)";
-#  git push;
+  git add .;
+  git commit -m "Evaluated experiment1&2 (version $v)";
+  git push;
 done
