090e5843cd564d85beb7ab6fb6f0c1ea6f79cf23
RubenPants <broekxruben@gmail.com>
2020-04-27 14:08:33 +0200

    Enabled LSTM in experiment1/2 and created a more memory-consious experiment2

--- experiment1.bash
+++ experiment1.bash
@@ -3,25 +3,27 @@
 # Default parameters
 iter=100;  # Number of training-iterations each loop
 cpu=1;  # Number of unused CPUs
+push=10;  # After how many version git should push
 
 # Run the program
-for v in {41..50}
+for v in {1..50}
 do
   for i in {1..5}
   do
-    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-    python3 experiment1.py --prob_gru=0.6 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0.6 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0.6 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-    python3 experiment1.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment1.py --prob_gru=0 --prob_simple_rnn=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment1.py --prob_gru=0.6 --prob_simple_rnn=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment1.py --prob_gru=0 --prob_simple_rnn=0.6 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+    python3 experiment1.py --prob_gru=0 --prob_simple_rnn=0 --prob_lstm=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
   done
-  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
-  python3 experiment2.py --prob_gru=0.6 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
-#  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0.6 --prob_gru_nu=0 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
-#  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0.6 --prob_simple_rnn=0 --version=$v --unused_cpu=$cpu;
-  python3 experiment2.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0.6 --version=$v --unused_cpu=$cpu;
+#  python3 experiment2.py --prob_gru=0 --prob_simple_rnn=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#  python3 experiment2.py --prob_gru=0.6 --prob_simple_rnn=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#  python3 experiment2.py --prob_gru=0 --prob_simple_rnn=0.6 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+  python3 experiment2.py --prob_gru=0 --prob_simple_rnn=0 --prob_lstm=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
 
-  git add .;
-  git commit -m "Evaluated experiment1&2 (version $v)";
-  git push;
+  if [ $(($v%$push)) == 0 ];  # Push every 10 generations
+  then
+    git add .;
+    git commit -m "Evaluated experiment1&2 (version $v)";
+    git push;
+  fi
 done
