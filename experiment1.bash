a1c02010685e19d5dda37cff0ceded1eb5c4d8ee
RubenPants <broekxruben@gmail.com>
2020-04-27 14:21:43 +0200

    Update experiment1.bash

--- experiment1.bash
+++ experiment1.bash
@@ -10,15 +10,15 @@ for v in {1..50}
 do
   for i in {1..5}
   do
-#    python3 experiment1.py --prob_gru=0 --prob_simple_rnn=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#    python3 experiment1.py --prob_gru=0.6 --prob_simple_rnn=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#    python3 experiment1.py --prob_gru=0 --prob_simple_rnn=0.6 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-    python3 experiment1.py --prob_gru=0 --prob_simple_rnn=0 --prob_lstm=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment1.py --prob_gru=0 --prob_sru=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment1.py --prob_gru=0.6 --prob_sru=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment1.py --prob_gru=0 --prob_sru=0.6 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+    python3 experiment1.py --prob_gru=0 --prob_sru=0 --prob_lstm=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
   done
-#  python3 experiment2.py --prob_gru=0 --prob_simple_rnn=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#  python3 experiment2.py --prob_gru=0.6 --prob_simple_rnn=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#  python3 experiment2.py --prob_gru=0 --prob_simple_rnn=0.6 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-  python3 experiment2.py --prob_gru=0 --prob_simple_rnn=0 --prob_lstm=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#  python3 experiment2.py --prob_gru=0 --prob_sru=0 --prob_lstm=0 --version=$v --unused_cpu=$cpu;
+#  python3 experiment2.py --prob_gru=0.6 --prob_sru=0 --prob_lstm=0 --version=$v --unused_cpu=$cpu;
+#  python3 experiment2.py --prob_gru=0 --prob_sru=0.6 --prob_lstm=0 --version=$v --unused_cpu=$cpu;
+  python3 experiment2.py --prob_gru=0 --prob_sru=0 --prob_lstm=0.6 --version=$v --unused_cpu=$cpu;
 
   if [ $(($v%$push)) == 0 ];  # Push every 10 generations
   then
