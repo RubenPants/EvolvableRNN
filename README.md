655f42e4f6205779e5936aee3950fc25774d2ded
RubenPants <broekxruben@gmail.com>
2020-04-27 21:42:47 +0200

    Moved files to backup and fixed bug in experiment6

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ Update a GRU his weights based on its current value, the value fed into the GRU 
 
 ## TODO
 
+* Create SRU with sigmoids, run locally perhaps?
+
 * Analyze model complexity across the populations (proof that GRU doesn't need a complex genome to be able to solve the problem)
 
 * Evaluate on OpenAI's gym environment (e.g. MountainCarContinuous-v0) to compare on another task? --> Would deviate too much from thesis I think
--- experiment6_2.py
+++ experiment6_2.py
@@ -145,7 +145,7 @@ def main(topology_id: int,
             with open(csv_path, 'a', newline='') as f:
                 writer = csv.writer(f)
                 for _, g in genomes:
-                    writer.writerow(get_genome_parameters(g))
+                    writer.writerow(get_genome_parameters(g, topology_id=topology_id))
             added += len(pop.population)
     except KeyboardInterrupt:
         # Remove the temporary CSV
@@ -260,7 +260,7 @@ def execution_test():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--evaluate', type=bool, default=True)  # Evaluate new genomes
-    parser.add_argument('--topology_id', type=int, default=1)  # ID of the used topology
+    parser.add_argument('--topology_id', type=int, default=2)  # ID of the used topology
     parser.add_argument('--batch', type=int, default=10)  # Number of genomes evaluated per batch
     parser.add_argument('--unused_cpu', type=int, default=2)  # Number of CPU cores not used during evaluation
     parser.add_argument('--save_population', type=bool, default=True)  # Save the final population after finishing
