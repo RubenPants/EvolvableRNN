9873abf5d942956594cff39f3e543db851aa1185
RubenPants <broekxruben@gmail.com>
2020-04-29 21:45:56 +0200

    Updated experiment3 to be LSTM compatible moved files to backup

--- experiment3.bash
+++ experiment3.bash
@@ -3,20 +3,23 @@
 # Default parameters
 iter=100;  # Number of training-iterations each loop
 cpu=1;  # Number of unused CPUs
+push=5;  # After how many version git should push
 
 # Run the program
-for v in {16..20}
+for v in {1..1}
   do
   for i in {1..5}
   do
-    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-    python3 experiment3.py --prob_gru=0.6 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0.6 --prob_gru_nu=0 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-#    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0.6 --prob_simple_rnn=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
-    python3 experiment3.py --prob_gru=0 --prob_gru_nr=0 --prob_gru_nu=0 --prob_simple_rnn=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment3.py --prob_gru=0 --prob_sru=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment3.py --prob_gru=0.6 --prob_sru=0 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+#    python3 experiment3.py --prob_gru=0 --prob_sru=0.6 --prob_lstm=0 --iterations=$iter --version=$v --unused_cpu=$cpu;
+    python3 experiment3.py --prob_gru=0 --prob_sru=0 --prob_lstm=0.6 --iterations=$iter --version=$v --unused_cpu=$cpu;
   done
 
-  git add .;
-  git commit -m "Ran experiment3 for version $v";
-  git push;
+  if [ $(($v%$push)) == 0 ];  # Push every 10 generations
+  then
+    git add .;
+    git commit -m "Evaluated experiment3 (version $v)";
+    git push;
+  fi
 done
