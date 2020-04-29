36280af804a0066f1c377c6b716cb32d8003ab4d
RubenPants <broekxruben@gmail.com>
2020-04-29 17:47:57 +0200

    Finished experiment1 and moved to backup

--- evaluation.bash
+++ evaluation.bash
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # Default parameters
-evaluate_gen=1;  # Evaluate the generations
-evaluate_pop=1;  # Combine population evaluations
-max_v=9;  # Maximum version for evaluation
+unused_cpu=2;  # Number of CPU threads not used  [bint]
+evaluate_gen=1;  # Evaluate the generations  [bint]
+evaluate_pop=1;  # Combine population evaluations  [bint]
+max_v=50;  # Maximum version for evaluation  [int]
 
 # Run the program
 for experiment in {1,2,}
 do
-#  python3 evaluate_populations.py --evaluate_gen=$evaluate_gen --evaluate_pop=$evaluate_pop --experiment=$experiment --folder_pop=NEAT --max_v=$max_v;
-  python3 evaluate_populations.py --evaluate_gen=$evaluate_gen --evaluate_pop=$evaluate_pop --experiment=$experiment --folder_pop=NEAT-GRU --max_v=$max_v;
-#  python3 evaluate_populations.py --evaluate_gen=$evaluate_gen --evaluate_pop=$evaluate_pop --experiment=$experiment --folder_pop=NEAT-LSTM --max_v=$max_v;
-#  python3 evaluate_populations.py --evaluate_gen=$evaluate_gen --evaluate_pop=$evaluate_pop --experiment=$experiment --folder_pop=NEAT-SRU --max_v=$max_v;
-
-#  git add .;
-#  git commit -m "Evaluated populations";
-#  git push;
+  python3 evaluate_populations.py --evaluate_gen=$evaluate_gen --evaluate_pop=$evaluate_pop --experiment=$experiment --folder_pop=NEAT --max_v=$max_v --unused_cpu=$unused_cpu;
+  python3 evaluate_populations.py --evaluate_gen=$evaluate_gen --evaluate_pop=$evaluate_pop --experiment=$experiment --folder_pop=NEAT-GRU --max_v=$max_v --unused_cpu=$unused_cpu;
+#  python3 evaluate_populations.py --evaluate_gen=$evaluate_gen --evaluate_pop=$evaluate_pop --experiment=$experiment --folder_pop=NEAT-LSTM --max_v=$max_v --unused_cpu=$unused_cpu;
+  python3 evaluate_populations.py --evaluate_gen=$evaluate_gen --evaluate_pop=$evaluate_pop --experiment=$experiment --folder_pop=NEAT-SRU --max_v=$max_v --unused_cpu=$unused_cpu;
 done
