32109ffe057ea9d0b4b54657240df7b5646d1d69
RubenPants <broekxruben@gmail.com>
2020-04-30 19:20:02 +0200

    Updated evaluation bash

--- evaluate_populations.py
+++ evaluate_populations.py
@@ -345,11 +345,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--evaluate_gen', type=int, default=0)
     parser.add_argument('--evaluate_pop', type=int, default=0)
-    parser.add_argument('--combine_pop', type=int, default=1)
+    parser.add_argument('--combine_pop', type=int, default=1)  # Goes over all the populations
     parser.add_argument('--evaluate_training', type=int, default=0)
-    parser.add_argument('--plot_distribution', type=int, default=0)
+    parser.add_argument('--plot_distribution', type=int, default=0)  # Goes over all the populations
     parser.add_argument('--test_correctness', type=bool, default=0)
-    parser.add_argument('--experiment', type=int, default=1)
+    parser.add_argument('--experiment', type=int, default=2)
     parser.add_argument('--folder', type=str, default=None)
     parser.add_argument('--folder_pop', type=str, default='NEAT')
     parser.add_argument('--max_v', type=int, default=50)
