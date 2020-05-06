1898e698fa64476ee2c443b7d2fbb111132cddd6
RubenPants <broekxruben@gmail.com>
2020-05-06 10:09:18 +0200

    Moved files to backup

--- main.py
+++ main.py
@@ -350,13 +350,13 @@ if __name__ == '__main__':
     parser.add_argument('--trace_fit', type=bool, default=False)
     parser.add_argument('--evaluate', type=bool, default=False)
     parser.add_argument('--genome', type=bool, default=False)
-    parser.add_argument('--monitor', type=bool, default=True)
+    parser.add_argument('--monitor', type=bool, default=False)
     parser.add_argument('--gru_analysis', type=bool, default=False)
-    parser.add_argument('--live', type=bool, default=False)
+    parser.add_argument('--live', type=bool, default=True)
     
     # Extra arguments
     parser.add_argument('--iterations', type=int, default=50)
-    parser.add_argument('--experiment', type=int, default=1)
+    parser.add_argument('--experiment', type=int, default=3)
     parser.add_argument('--unused_cpu', type=int, default=2)
     parser.add_argument('--version', type=int, default=0)
     parser.add_argument('--debug', type=bool, default=False)
@@ -375,7 +375,7 @@ if __name__ == '__main__':
     
     # Setup the population
     pop = Population(
-            name='NEAT-SRU/v2',
+            name='NEAT-GRU/v3',
             # name=get_name(cfg=config, version=args.version),
             # folder_name='experiment1',
             folder_name=get_folder(args.experiment),
