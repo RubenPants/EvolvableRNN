a867c36a6d3cff10f6bee7344f8b94ed9b2af3dc
RubenPants <broekxruben@gmail.com>
2020-05-11 22:29:54 +0200

    Created experiment 7

---
+++ experiment7.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Default parameters
+topology=3;  # Topology-ID
+iter=100;  # Number of training-iterations each loop
+cpu=1;  # Number of unused CPUs
+push=10;  # After how many version git should push
+
+# Run the program
+for v in {1..10}
+do
+  python3 experiment7.py --topology_id=$topology --version=$v --iterations=$iter --unused_cpu=$cpu;
+
+  if [ $(($v%$push)) == 0 ];  # Push every 'push' populations
+  then
+    git add .;
+    git commit -m "Evaluated experiment7 (version $v)";
+    git push;
+  fi
+done
