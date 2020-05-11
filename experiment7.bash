daa60244566fa104e03e8b6225502ce377d582c1
RubenPants <broekxruben@gmail.com>
2020-05-11 22:38:06 +0200

    Update experiment7.bash

--- experiment7.bash
+++ experiment7.bash
@@ -7,7 +7,7 @@ cpu=1;  # Number of unused CPUs
 push=10;  # After how many version git should push
 
 # Run the program
-for v in {11..20}
+for v in {21..30}
 do
   python3 experiment7.py --topology_id=$topology --version=$v --iterations=$iter --unused_cpu=$cpu;
 
