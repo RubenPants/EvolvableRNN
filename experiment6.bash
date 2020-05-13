7d9b09c63a4eeda6ce712919566015468037887f
RubenPants <broekxruben@gmail.com>
2020-05-13 12:41:42 +0200

    Moved files to backup

--- experiment6.bash
+++ experiment6.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Default parameters
-batch=100000;  # Number of training-iterations each loop
+batch=300000;  # Number of training-iterations each loop
 cpu=2;  # Number of unused CPUs
 
 # Run the program
-for t in {22,33,}
+for t in {22,}
 do
   python3 experiment6_2.py --topology_id=$t --batch=$batch --unused_cpu=$cpu;
 
