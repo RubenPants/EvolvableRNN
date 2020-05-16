dbb3b80e58853280f869c24644989863589a8102
RubenPants <broekxruben@gmail.com>
2020-05-16 23:17:31 +0200

    Update experiment7.bash

--- experiment7.bash
+++ experiment7.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default parameters
-pop_name=connection;  # Topology-ID
+pop_name=default;  # Topology-ID
 cpu=1;  # Number of unused CPUs
 push=10;  # After how many version git should push
 
