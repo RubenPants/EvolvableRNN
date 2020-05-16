7f0727ab520eb33b7aab95a2eb72caaa440265b5
RubenPants <broekxruben@gmail.com>
2020-05-16 12:02:33 +0200

    Update experiment7.bash

--- experiment7.bash
+++ experiment7.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default parameters
-pop_name=gru_nr;  # Topology-ID
+pop_name=gru_nr_connection;  # Topology-ID
 cpu=1;  # Number of unused CPUs
 push=10;  # After how many version git should push
 
@@ -10,10 +10,10 @@ for v in {1..50}
 do
   python3 experiment7.py --pop_name=$pop_name --version=$v --unused_cpu=$cpu;
 
-#  if [ $(($v%$push)) == 0 ];  # Push every 'push' populations
-#  then
-#    git add .;
-#    git commit -m "Evaluated experiment7 (version $v)";
-#    git push;
-#  fi
+  if [ $(($v%$push)) == 0 ];  # Push every 'push' populations
+  then
+    git add .;
+    git commit -m "Evaluated experiment7 (version $v)";
+    git push;
+  fi
 done
