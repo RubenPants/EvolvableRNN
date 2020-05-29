3f958322ea9d78a7fe699f87d5ce70aab3b563f2
RubenPants <broekxruben@gmail.com>
2020-05-29 09:17:17 +0200

    Update experiment8.bash

--- experiment8.bash
+++ experiment8.bash
@@ -5,7 +5,7 @@ cpu=1;  # Number of unused CPUs
 push=10;  # After how many version git should push
 
 # Run the program
-for v in {4..10}
+for v in {24..30}
 do
   for pop_name in default gru_nr biased
   do
